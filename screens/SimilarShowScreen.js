bb0fdff55e1b2017b46a6b2d043e342322b2586c
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2019-12-01 21:01:47 +0100

    [Depends on #2] Empty screens (#3)
    * Setup for precommit eslint with prettier integrated
    
    * Create empty screens related to tv shows

---
+++ screens/SimilarShowScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { generalStyles } from "../shared/styles";
+
+export const SimilarShowsScreen = props => (
+  <View style={generalStyles.screen}>
+    <Text>Similar shows screen</Text>
+  </View>
+);
